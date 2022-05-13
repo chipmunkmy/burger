2ccaf733cb2580ea95615c6e145bb14325e991f6
Anne Hedges <norhafizah.my@gmail.com>
2022-05-13 23:19:06 +0800

    Merge branch 'master' of https://github.com/chipmunkmy/ztm-template

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
       <h1 class="text-5xl">Welcome to Webdevtech Inc!</h1>
-      <p class="mt-5">Where technologies and creativity colour our life.</p>
+      <p class="mt-5">
+        Where technologies and creativity colour our life. Makes life easier
+      </p>
     </div>
   </div>
 </template>
