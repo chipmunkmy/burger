73a6d459bf0e5fa57d9dacefb7a03e2ca4fd2688
Anne Hedges <norhafizah.my@gmail.com>
2022-05-14 17:01:07 +0800

    emailupdate

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -29,7 +29,7 @@ exports.handler = async function (event, context) {
             secure: true,
             auth: {
                 user: "apiKey",
-                pass: process.env.apiKey
+                pass: process.env.apiKey,
             }
         });
 
