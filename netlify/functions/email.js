f1cb093a5c67cf6b99be10e020cf4e3e6f32b5bd
Anne Hedges <norhafizah.my@gmail.com>
2022-05-16 21:42:48 +0800

    update email

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -17,7 +17,7 @@ exports.handler = async function (event, context) {
         emailContent = emailContent + '\n Total Amount: ${total.toFixed(2)}';
 
         const email = {
-            from: "norhafizah@ppas.gov.my",
+            from: "norhafizah.my@gmail.com",
             to: customerEmail,
             subject: "New Order Received",
             text: emailContent,
