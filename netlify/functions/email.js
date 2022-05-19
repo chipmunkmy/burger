2a998491c56b62a65e6adbc765c2a526e3796586
Anne Hedges <norhafizah.my@gmail.com>
2022-05-20 04:16:01 +0800

    update email

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+const mailjetTransport = require('nodemailer-mailjet-transport');
 
 exports.handler = async function (event, context) {
     const body = JSON.parse(event.body);
@@ -23,7 +24,7 @@ exports.handler = async function (event, context) {
             text: emailContent,
         };
 
-        const mailer = nodemailer.createTransport({
+        const mailer = nodemailer.createTransport(mailjetTransport({
             host: 'in-v3.mailjet.com',
             port: 465,
             secure: true,
@@ -31,7 +32,7 @@ exports.handler = async function (event, context) {
                 user: process.env.MailJetAPIKey,
                 pass: process.env.MailJetAPISecretKey
             }
-        });
+        }));
 
         try {
             await mailer.sendMail(email);
