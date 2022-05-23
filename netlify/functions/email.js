98d66ef4376e19b87ce983a5dc90a855d07cc401
Anne Hedges <norhafizah.my@gmail.com>
2022-05-24 07:22:39 +0800

    update code copied

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -9,38 +9,43 @@ exports.handler = async function (event, context) {
     let emailContent = "We have received a new order: \n\n";
 
     orders.forEach((order) => {
-        emailContent = emailContent +
-        '${order.name} - ${order.quantity} pcs - ${order.price * order.quantity}\n';
-        total = total + order.price * orders.quantity; });
+        emailContent =
+        emailContent +
+        `${order.name} - ${order.quantity} pcs - ${
+            order.price * order.quantity
+        }\n`;
+        total = total + order.price * order.quantity;
+    });
 
-        emailContent = emailContent + '\n Total Amount: ${total.toFixed(2)}';
+    emailContent = emailContent + `\n Total Amount: ${total.toFixed(2)}`;
 
-        const email = {
-            from: "norhafizah.my@gmail.com",
-            to: customerEmail,
-            subject: "New Order Received",
-            text: emailContent,
-        };
-
-        const mailer = nodemailer.createTransport({
-            host: 'in-v3.mailjet.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.MailJetAPIKey,
-                pass: process.env.MailJetAPISecretKey
-            }
-        });
-
-        try {
-            await mailer.sendMail(email);
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: "Email sent successfully",
-                }),
-            };
-        } catch (error) {
-            console.log("Error sending email", error);
-        }
+    const email = {
+        from: "norhafizah.my@gmail.com",
+        to: customerEmail,
+        subject: "New Order Received",
+        text: emailContent,
     };
+
+    const mailer = nodemailer.createTransport({
+        host: 'in-v3.mailjet.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.MailJetAPIKey,
+            pass: process.env.MailJetAPISecretKey
+        }
+    });
+
+    try {
+        await mailer.sendMail(email);
+        return {
+            statuscode: 200,
+            body: JSON.stringify({
+                message: "Email sent successfully",
+            }),
+        };
+    }
+    catch (error) {
+        console.log('Error sending email', error);
+    }
+};
