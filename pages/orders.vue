31ddfa2e05f38d388eb5e0c135a592b2baa563fc
Anne Hedges <norhafizah.my@gmail.com>
2022-05-30 23:27:03 +0800

    update readorders and orders for netlify

--- pages/orders.vue
+++ pages/orders.vue
@@ -103,9 +103,15 @@ export default {
 
   methods: {
     readOders() {
-      this.$axios.get('/.netlify/functions/readorders').then((response) => {
-        this.orders = response.data;
-      });
+      this.$axios
+        .get('/.netlify/functions/readorders', {
+          headers: {
+            Authorization: 'Bearer $this{.user.token.access_token}',
+          },
+        })
+        .then((response) => {
+          this.orders = response.data;
+        });
     },
 
     login() {
